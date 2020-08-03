db0ae2703c538302b0c9306752192ed51dfb591a
skhabiri <shahnam.khabiri@gmail.com>
2020-08-03 15:11:44 -0700

    cleaned up the init file

--- lambdata_skhabiri/__init__.py
+++ lambdata_skhabiri/__init__.py
@@ -7,9 +7,3 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.metrics import accuracy_score, mean_absolute_error
 from sklearn.metrics import plot_confusion_matrix, classification_report, confusion_matrix
-
-######text
-# import pandas as pd
-# import numpy as np
-# from lambdata_skhabiri.dataframe_helper import report_missing_values
-# TEST = pd.DataFrame(np.ones(10))
