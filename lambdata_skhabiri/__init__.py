0fe23d9cad661d40895e33693af2d13baebfc505
skhabiri <shahnam.khabiri@gmail.com>
2020-08-03 14:56:07 -0700

    report fornction fixed

--- lambdata_skhabiri/__init__.py
+++ lambdata_skhabiri/__init__.py
@@ -8,6 +8,7 @@ import matplotlib.pyplot as plt
 from sklearn.metrics import accuracy_score, mean_absolute_error
 from sklearn.metrics import plot_confusion_matrix, classification_report, confusion_matrix
 
+######text
 # import pandas as pd
 # import numpy as np
 # from lambdata_skhabiri.dataframe_helper import report_missing_values
--- lambdata_skhabiri/dataframe_helper.py
+++ lambdata_skhabiri/dataframe_helper.py
@@ -4,6 +4,6 @@ some comments
 
 import pandas as pd
 
-def reporting_missing_values(df):
+def report_missing_values(df):
     """Print a pretty report of missing values."""
     print('Need to write code here!')
