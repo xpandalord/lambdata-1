e166480aba36e20f687bb0af1f0eee2dd87d5b4b
skhabiri <shahnam.khabiri@gmail.com>
2020-08-03 14:16:35 -0700

    fit_est modified

--- lambdata_skhabiri/fit_est.py
+++ lambdata_skhabiri/fit_est.py
@@ -1,11 +1,11 @@
 """
 Get a dataframe and print some statistics about it.
 """
-# import pandas as pd
-# import numpy as np
-# import matplotlib.pyplot as plt
-# from sklearn.metrics import accuracy_score, mean_absolute_error
-# from sklearn.metrics import plot_confusion_matrix, classification_report, confusion_matrix
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.metrics import accuracy_score, mean_absolute_error
+from sklearn.metrics import plot_confusion_matrix, classification_report, confusion_matrix
 
 
 
