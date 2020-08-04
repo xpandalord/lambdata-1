2f3304e97fe2429d4ec323789680a95c1d533a38
skhabiri <shahnam.khabiri@gmail.com>
2020-08-03 17:02:20 -0700

    changed the version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open("README.md", "r") as file:
     LONG_DESCRIPTION = file.read()
 setuptools.setup(
     name="lambdata-skhabiri",
-    version="0.0.6",
+    version="1.0.0",
     author="skhabiri",
     description="fit estimate functions",
     long_description=LONG_DESCRIPTION,
