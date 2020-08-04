532dc45ac8059011d401b0d42b8bc4393770a1d7
skhabiri <shahnam.khabiri@gmail.com>
2020-08-03 23:20:32 -0700

    updated

--- setup.py
+++ setup.py
@@ -11,8 +11,8 @@ REQUIRED = [
 with open("README.md", "r") as file:
     LONG_DESCRIPTION = file.read()
 setuptools.setup(
-    name="lambdata-shahnam",
-    version="0.0.8",
+    name="lambdata-skhabiri1",
+    version="0.0.1",
     author="skhabiri",
     description="fit estimate functions",
     long_description=LONG_DESCRIPTION,
